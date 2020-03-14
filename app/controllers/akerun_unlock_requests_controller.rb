894bf5efb83d1d8b276cc5a97c735c28a32f9c15
baramatsubonzo <baramatsubonzo@gmail.com>
2020-03-14 18:25:10 +0900

    特定のSlackワークスペースからのアクセスのみ許可する

--- app/controllers/akerun_unlock_requests_controller.rb
+++ app/controllers/akerun_unlock_requests_controller.rb
@@ -1,5 +1,18 @@
 class AkerunUnlockRequestsController < ApplicationController
+  before_action :reject_no_authorized_clients
+
   def create
     render plain: AkerunUnlockRequest.new.adjust_value_from_akerun_api
   end
+
+  private
+    def reject_no_authorized_clients
+      # RFC: 今回Slack推奨のsigning secretの利用で動作確認できず、非推奨であるがverification tokenを利用。
+      render status: 404 if confirm_not_slack_token
+    end
+
+    def confirm_not_slack_token
+      # TODO: Env.fetch('SLACK_TOKEN')へ変更する。
+      request.params['token'] != 'hogehoge'
+    end
 end
