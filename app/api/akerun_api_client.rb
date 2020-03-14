81339bda912add880bd40a8da783040064dc46b9
baramatsubonzo <baramatsubonzo@gmail.com>
2020-03-14 15:53:36 +0900

    AkerunAPIへ解錠リクエストを送信するメソッド

--- app/api/akerun_api_client.rb
+++ app/api/akerun_api_client.rb
@@ -1,2 +1,11 @@
-class AkerunApiClient
+class AkerunApiClient < HTTPClient
+  AKERUN_BASE_URI = "https://api.akerun.com/v3/organizations/#{ENV.fetch('ORGANIZATION_ID')}"
+
+  def initialize
+    super(default_header: { 'Authorization' => ENV.fetch('AKERUN_ACCESS_TOKEN') })
+  end
+
+  def post_unlock_request
+    post("#{AKERUN_BASE_URI}/akeruns/#{ENV.fetch('AKERUN_ID')}/jobs/unlock")
+  end
 end
--- spec/api/akerun_api_client_spec.rb
+++ spec/api/akerun_api_client_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
+WebMock.enable! 
 
 RSpec.describe AkerunApiClient do
+  AKERUN_BASE_URI = "https://api.akerun.com/v3/organizations/#{ENV.fetch('ORGANIZATION_ID')}"
+  let(:client){ AkerunApiClient.new }
+  
+  describe 'post_unlock_request' do
+    before do
+      stub_request(:post, "#{AKERUN_BASE_URI}/akeruns/#{ENV.fetch('AKERUN_ID')}/jobs/unlock")
+        .to_return(status: 201)
+    end
+    it 'return status 201' do
+      res = client.post_unlock_request
+      expect(res.status).to eq 201
+    end
+  end
 end 
